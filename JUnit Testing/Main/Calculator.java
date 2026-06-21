f93c85f26091980f80827c25e198993954eba04d
Vaibhav Sanghani <167650810+vaibhavsanghani-95@users.noreply.github.com>
2026-06-21 19:45:43 +0530

    Add files via upload

---
+++ JUnit Testing/Main/Calculator.java
@@ -0,0 +1,10 @@
+package org.project.junittesting;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+}
